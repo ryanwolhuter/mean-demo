bb31e1ca7c45a74736a45aefbab72e310dccb516
ryanwolhuter <dev@ryanwolhuter.com>
2019-05-16 12:56:38 +0200

    Add contact details component

--- src/app/contacts/contact-details/contact-details.component.ts
+++ src/app/contacts/contact-details/contact-details.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Contact } from '../contact';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
   styleUrls: ['./contact-details.component.scss']
 })
-export class ContactDetailsComponent implements OnInit {
+export class ContactDetailsComponent {
+  @Input()
+  contact: Contact;
 
-  constructor() { }
+  @Input()
+  createHandler: (newContact: Contact) => void;
+  @Input()
+  updateHandler: (updatedContact: Contact) => void;
+  @Input()
+  deleteHandler: (deletedContactId: string) => void;
 
-  ngOnInit() {
+  constructor(private contactService: ContactService) { }
+
+  createContact(contact: Contact) {
+    this.contactService.createContact(contact).then((newContact: Contact) => {
+      this.createHandler(newContact);
+    });
   }
 
+  updateContact(contact: Contact): void {
+    this.contactService.updateContact(contact).then((updatedContact: Contact) => {
+      this.updateHandler(updatedContact);
+    });
+  }
+
+  deleteContact(contactId: string): void {
+    this.contactService.deleteContact(contactId).then((deletedContactId: string) => {
+      this.deleteHandler(deletedContactId);
+    });
+  }
 }
